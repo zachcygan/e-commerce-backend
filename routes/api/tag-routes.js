3c72ac6db5b11b4243b83e9427a259cbffa8b186
Zachary Cygan <zachcygan@gmail.com>
2023-03-27 10:36:14 -0700

    test commit

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,6 +18,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
