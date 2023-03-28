19143937993dc946ac46d8a400c9d0b12b8b8487
Zachary Cygan <zachcygan@gmail.com>
2023-03-27 22:05:53 -0700

    fixed deleting a product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,6 +114,12 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
+    await ProductTag.destroy({
+      where: {
+        product_id: req.params.id,
+      }
+    })
+
     const productData = await Product.destroy({
       where: {
         id: req.params.id
